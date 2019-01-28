1d2c6c431ee9af2602ee7dd6129af36a4d453cae
Unknown <guardeivid@yahoo.com.ar>
2019-01-28 13:59:09 -0300

    cors *

--- app/routes.php
+++ app/routes.php
@@ -8,8 +8,9 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
 
 $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
+    //  'http://localhost:8080'
     return $response
-        ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
+        ->withHeader('Access-Control-Allow-Origin', '*')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
